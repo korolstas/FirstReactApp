049a7e798728ccb639b743fe1ac39f8d88e6c8f2
korolstas <1st.korollstas@mail.ru>
2023-05-24 12:27:58 +0300

    изменение класса Routes на стрелочную ф

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
@@ -7,13 +6,13 @@ import Head from './components/header/index';
 
 function App() {
   return (
-	<div className="App">
-		<BrowserRouter>
-			<Head/>
-			<Routers/>
-		</BrowserRouter>
-	</div>
-	);
+    <div className="App">
+      <BrowserRouter>
+        <Head />
+        <Routers />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/routers.tsx
+++ src/routers.tsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './pages/Header/home';
 import Form from './pages/Form/form';
 import About from './pages/About/about';
 import Error from './pages/Error 404/index';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-export default class Routers extends Component {
-  render() {
-    return (
+const Routers = () => {
+  return (
+    <div className="container_1e12">
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="about" element={<About />} />
@@ -15,6 +15,7 @@ export default class Routers extends Component {
         <Route path="error" element={<Error />} />
         <Route path="*" element={<Navigate to="/error" replace />} />
       </Routes>
-    );
-  }
-}
+    </div>
+  );
+};
+export default Routers;
