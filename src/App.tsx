46e0d9274f11703704c4d230af48b5d73e7d2497
korolstas <1st.korollstas@mail.ru>
2023-05-30 12:14:39 +0300

    добавил loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routers from './routers';
 import Header from './components/Header/Header';
+import Loader from './components/Loader/Loader';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const delay = 5000; // время задержки в миллисекундах
+
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, delay);
+
+    // Очистка таймера при размонтировании компонента
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Routers />
+        {isLoading ? <Loader /> : <Routers />}
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './loader.css';
+
+const Loader = () => {
+  return (
+    <div className="content">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+};
+
+export default Loader;
